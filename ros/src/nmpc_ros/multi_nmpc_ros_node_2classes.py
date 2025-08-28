2cb3c59ab696cf7163099abcefcb4e7f93b40971
Andrea <andreamiele120@gmail.com>
2025-08-28 17:19:36 +0200

    2classes no connectivity

---
+++ ros/src/nmpc_ros/multi_nmpc_ros_node_2classes.py
@@ -0,0 +1,12 @@
+import sys
+import os
+script_dir = os.path.dirname(os.path.abspath(__file__))  
+src_path = os.path.join(script_dir, "src")  
+sys.path.append(src_path)
+from nmpc_multi_2classes import NeuralMPC
+
+if __name__ == '__main__':
+    # Run the simulation with the default parameters
+    mpc = NeuralMPC()
+    all_trajectories, entropy_history, lambda_history, durations, g_nn_raw, g_nn_ripe, trees_pos, lb, ub =  mpc.run_simulation()
+    entropy_mpc_pred = mpc.plot_animated_trajectory_and_entropy_2d(all_trajectories, entropy_history, lambda_history, trees_pos, lb, ub, durations)
